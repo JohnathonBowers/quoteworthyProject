cbca67b951ea30382a9c8853e64147a92d205353
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-07 21:21:44 -0400

    added delete route for deleting one quotation in quotation.routes.js

--- server/routes/quotation.routes.js
+++ server/routes/quotation.routes.js
@@ -2,4 +2,5 @@ const QuotationController = require("../controllers/quotation.controller")
 
 module.exports = app => {
     app.post("/api/quotations", QuotationController.createNewQuotation)
+    app.delete("/api/quotations/:id", QuotationController.deleteOneQuotation)
 }
