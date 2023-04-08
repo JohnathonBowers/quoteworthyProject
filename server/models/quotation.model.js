f6c1ed00d3ace1b28a4e30415d392e23e640561b
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-07 20:47:48 -0400

    added yearPublished field and end page validator to quotation model

--- server/models/quotation.model.js
+++ server/models/quotation.model.js
@@ -31,6 +31,12 @@ const QuotationSchema = new mongoose.Schema({
         minlength: [2, "Publisher location must be at least two characters long."],
         maxlength: [30, "Publisher location must not be longer than 30 characters."]
     },
+    yearPublished: {
+        type: String,
+        required: [true, "Year of publication is required."],
+        min: [1, "Year must be 1 or higher."],
+        max: [2050, "Year must be 2050 or below."]
+    },
     startPage: {
         type: Number,
         required: [true, "Start page is required."],
@@ -40,6 +46,12 @@ const QuotationSchema = new mongoose.Schema({
     endPage: {
         type: Number,
         required: [true, "End page is required."],
+        validate: {
+            validator: function endPageValidator(value) {
+                return value >= this.startPage;
+            },
+            message: "End page must be greater than or equal to start page."
+        },
         min: [1, "End page must be 1 or higher."],
         max: [10000, "End page must not be greater than 10,000."]
     },
