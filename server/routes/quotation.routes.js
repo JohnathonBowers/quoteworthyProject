3b33f2dedb57b54dde00cb0a15f9843544f9120e
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-07 21:48:27 -0400

    added route for finding all quotations in quotation.routes.js

--- server/routes/quotation.routes.js
+++ server/routes/quotation.routes.js
@@ -1,6 +1,7 @@
 const QuotationController = require("../controllers/quotation.controller")
 
 module.exports = app => {
+    app.get("/api/quotations", QuotationController.findAllQuotations)
     app.post("/api/quotations", QuotationController.createNewQuotation)
     app.delete("/api/quotations/:id", QuotationController.deleteOneQuotation)
 }
