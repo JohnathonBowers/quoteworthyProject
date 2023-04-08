bf03511c6fe862b74fac7515952def7609f874f1
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-07 21:19:49 -0400

    added deleteOneQuotation function to quotation.controller.js

--- server/controllers/quotation.controller.js
+++ server/controllers/quotation.controller.js
@@ -6,5 +6,11 @@ module.exports = {
         Quotation.create(req.body)
             .then(createdQuotation => res.json(createdQuotation))
             .catch(err => res.status(400).json(err))
+    },
+    // Delete one quotation
+    deleteOneQuotation: (req, res) => {
+        Quotation.findByIdAndDelete(req.params.id)
+            .then(deleteConfirmation => res.json(deleteConfirmation))
+            .catch(err => res.status(400).json(err))
     }
 }
