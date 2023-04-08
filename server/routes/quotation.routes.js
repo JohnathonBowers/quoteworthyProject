9c7b16cd6c9f0e07fc670b54c51aeb8c4effc1a1
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-07 21:53:13 -0400

    added route for finding one quotation in quotation.routes.js

--- server/routes/quotation.routes.js
+++ server/routes/quotation.routes.js
@@ -2,6 +2,7 @@ const QuotationController = require("../controllers/quotation.controller")
 
 module.exports = app => {
     app.get("/api/quotations", QuotationController.findAllQuotations)
+    app.get("/api/quotations/:id", QuotationController.findOneQuotation)
     app.post("/api/quotations", QuotationController.createNewQuotation)
     app.delete("/api/quotations/:id", QuotationController.deleteOneQuotation)
 }
