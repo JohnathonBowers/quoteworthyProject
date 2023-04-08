c43886dca243ffedc89c22867b65fe025fee3df1
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-07 21:57:39 -0400

    added route for updating one quotation in quotation.routes.js

--- server/routes/quotation.routes.js
+++ server/routes/quotation.routes.js
@@ -4,5 +4,6 @@ module.exports = app => {
     app.get("/api/quotations", QuotationController.findAllQuotations)
     app.get("/api/quotations/:id", QuotationController.findOneQuotation)
     app.post("/api/quotations", QuotationController.createNewQuotation)
+    app.put("/api/quotations/:id", QuotationController.updateOneQuotation)
     app.delete("/api/quotations/:id", QuotationController.deleteOneQuotation)
 }
