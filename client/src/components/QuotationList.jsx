5c97246067b16aa86f6db7251cd3cbb2b4758705
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-08 21:41:40 -0400

    built out a base layout for the QuotationList component

--- client/src/components/QuotationList.jsx
+++ client/src/components/QuotationList.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const QuotationList = () => {
+    
+    const [quotations, setQuotations] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/quotations")
+            .then(res => setQuotations(res.data))
+            .catch(err => console.log(err))
+    }, [])
+    
     return (
-        <div>
-            Hello World
+        <div className="container mt-4 col-sm-8">
+            <div className="d-flex flex-row justify-content-between align-items-center mb-4">
+                <h2><strong>Quoteworthy</strong></h2>
+                <Link to="/add">Add Quotation</Link>
+            </div>
+            <div className="d-flex flex-row justify-content-center">
+                <h2 className="my-4">Your Collection</h2>
+            </div>
+            <div>
+                {
+                    quotations.map((quotation, index) => {
+                        return (
+                            <div key={index}>
+                                <p className="mb-2"><strong>{quotation.authorLastName}, {quotation.authorFirstName}</strong></p>
+                                <div className="row">
+                                    <div className="col-sm-10">
+                                        <p className="mb-2">"{quotation.quotation}"</p>
+                                        {
+                                            quotation.endPage === quotation.startPage ?
+                                            <p className="text-end"><em>{quotation.bookTitle}</em>, {quotation.startPage}</p> :
+                                            <p className="text-end"><em>{quotation.bookTitle}</em>, {quotation.startPage} - {quotation.endPage}</p>
+                                        }
+                                    </div>
+                                    <div className="d-flex flex-column justify-content-evenly align-items-center col-sm-2">
+                                        <p>Details</p>
+                                        <p>Edit</p>
+                                        <p>Delete</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
