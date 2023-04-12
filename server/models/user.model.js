f014a1b3db99ed85abab3d7e2d777f73e092ec54
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-12 14:31:14 -0400

    created user.models.js file and added UserSchema

---
+++ server/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const {isEmail, isStrongPassword} = require('validator');
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required."],
+        maxlength: [30, "First name must not be longer than 30 characters."]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required."],
+        maxlength: [30, "Last name must not be longer than 30 characters."]
+    },
+    email: {
+        type: String,
+        required: [true, "Email address is required."],
+        unique: [true, "This email address is already associated with an account!"],
+        validate: [isEmail, "Please enter a valid email address."],
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."],
+        minlength: [8, "Password must be eight characters or longer."],
+        maxlength: [20, "Password must not be longer than 20 characters."]
+    }
+}, {timestamps: true});
+
+UserSchema.virtual("confirmPassword")
+    .get(() => this._confirmPassword)
+    .set(value => this._confirmPassword = value)
+
+UserSchema.pre("validate", function(next) {
+    if(this.password !== this.confirmPassword) {
+        this.invalidate("confirmPassword", "Passwords do not match!")
+    }
+    next();
+});
+
+UserSchema.pre("save", function(next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+            next();
+        });
+});
+
+module.exports = mongoose.model("User", UserSchema)
