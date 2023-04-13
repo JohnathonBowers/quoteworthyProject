06235705a0b6a09bc1180a788d100d5f44226073
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-12 21:11:31 -0400

    created loginUser function in user.controller.js file

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,4 +1,7 @@
 const User = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const bcrypt = require('bcrypt')
+const secret = process.env.SECRET_KEY
 
 module.exports = {
     // Create a new user
@@ -6,14 +9,35 @@ module.exports = {
         try {
             const potentialUser = await User.findOne({email: req.body.email});
             if (potentialUser) {
-                return res.status(400).json({message: "The email you entered is already associated with an account!"})
+                res.status(400).json({message: "The email you entered is already associated with an account!"})
             } else {
                 const newUser = await User.create(req.body)
-                return res.json({message: "Success!", user: newUser })
+                const userToken = jwt.sign({id: newUser._id}, secret)
+                res.cookie("usertoken", userToken, {httpOnly: true}).json({message: "success", user: newUser})
             }
         } catch (err) {
             console.log(err)
             return res.status(400).json(err)
         }
-    }    
+    },
+    // Log in a user
+    loginUser: async (req, res) => {
+        try {
+            const user = await User.findOne({email: req.body.email});
+            if (user) {
+                const passwordMatch = await bcrypt.compare(req.body.password, user.password)
+                if (passwordMatch) {
+                    const userToken = jwt.sign({id: user._id}, secret)
+                    res.cookie("usertoken", userToken, {httpOnly: true}).json({message: "success", user: user})
+                } else {
+                    res.status(400).json({message: "Invalid login attempt"})
+                }
+            } else {
+                res.status(400).json({message: "Invalid login attempt"})
+            }
+        } catch (err) {
+            console.log(err)
+            return res.status(400).json(err)
+        }
+    }
 }
