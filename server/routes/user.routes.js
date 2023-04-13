5d685b49a7e5984e3d212ab814a45510b78dc526
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-12 21:12:15 -0400

    added post route for user login

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,4 +2,5 @@ const UserController = require("../controllers/user.controllers")
 
 module.exports = app => {
     app.post("/api/users/register", UserController.registerUser)
+    app.post("/api/users/login", UserController.loginUser)
 }
