0b8d728fbe09899b01efc21e74b80384b1ac2070
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-15 11:31:42 -0400

    added functions and routes to allow me to view and delete users

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,4 +4,6 @@ module.exports = app => {
     app.post("/api/users/register", UserController.registerUser)
     app.post("/api/users/login", UserController.loginUser)
     app.post("/api/users/logout", UserController.logoutUser)
+    app.get("/api/users", UserController.findAllUsers)
+    app.delete("/api/users/:id", UserController.deleteOneUser)
 }
